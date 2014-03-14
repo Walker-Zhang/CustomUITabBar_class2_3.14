20f3c4d89354cd0dbb1da1111f17c386013f06e2
xiang_jj <xiang_jj@xiang-jjtekiMacBook-Air.local>
2014-03-14 14:32:11 +0800

    Initial Commit

---
+++ CustomUITabBar_class2_3.14Tests/CustomUITabBar_class2_3_14Tests.m
@@ -0,0 +1,34 @@
+//
+//  CustomUITabBar_class2_3_14Tests.m
+//  CustomUITabBar_class2_3.14Tests
+//
+//  Created by xiang_jj on 14-3-14.
+//  Copyright (c) 2014年 xiang_jj. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CustomUITabBar_class2_3_14Tests : XCTestCase
+
+@end
+
+@implementation CustomUITabBar_class2_3_14Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
